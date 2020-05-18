315fc182fe51dc2d3e4f58327c6f8059b02e90da
pankajwp <pankaj151290mech@gmail.com>
2020-05-19 00:45:23 +0530

    with service and about

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.status(200).send('hello world')
+	res.status(200).send('hello world master')
+})
+
+app.get('/service', (req, res) => {
+	res.status(200).send('service');
+})
+
+app.get('/about', (req, res) => {
+	res.status(200).send('about-us');
 })
 
 
